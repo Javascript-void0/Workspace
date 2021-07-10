6ea2c5260210ebe48aebfa0db6a85e51a67320ad
Java <indead1124@gmail.com>
2021-07-10 19:41:53 -0400

    Update mochi2.py

--- mochi2.py
+++ mochi2.py
@@ -2,20 +2,36 @@ import keyboard
 import time
 print('Started')
 
+# while True:
+#     keyboard.wait('ctrl+enter')
+#     time.sleep(0.1)
+#     keyboard.write('''}}
+
+# () 
+# > 
+
+# Synonym: 
+# Antonym: ''')
+#     keyboard.press_and_release('left')
+#     time.sleep(0.1)
+#     for i in range(6):
+#         keyboard.press_and_release('up')
+#     time.sleep(0.1)
+#     keyboard.write('### {{')
+#     print('Setup New Card')
+
 while True:
     keyboard.wait('ctrl+enter')
     time.sleep(0.1)
-    keyboard.write('''}}
+    keyboard.write('''### {{}}
 
 () 
 > 
 
 Synonym: 
 Antonym: ''')
-    keyboard.press_and_release('left')
     time.sleep(0.1)
-    for i in range(6):
+    for i in range(12):
+        keyboard.press_and_release('left')
         keyboard.press_and_release('up')
-    time.sleep(0.1)
-    keyboard.write('### {{')
     print('Setup New Card')
